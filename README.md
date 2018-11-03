0cde1197a6d1175f119b2262bff39e400a9c13c9
nemo <git@nemo.ikkoku.de>
2018-11-04 00:09:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+- models: contains pre-trained models, such as the librispeech
+  	      DeepSpeech.pytorch model
---
+++ data/dataset.py
@@ -0,0 +1,62 @@
+import os
+from itertools import cycle
+
+
+DEFAULT_PROVIDERS = {
+    'dictcc': {
+        'path': 'distcc/download/',
+        'format': 'mp3',
+    },
+    'forvo': {
+        'path': 'forvo/download/',
+        'format': 'mp3',
+    },
+    'meariamwebster': {
+        'path': 'meriamwebster/download/',
+        'format': 'wav',
+    },
+}
+
+
+class SwearDataset:
+    def __init__(self, providers, good_word_path='good_words.txt', bad_word_path='bad_words.txt'):
+        self.good_word_path = good_word_path
+        self.bad_word_path = bad_word_path
+        self.providers = providers
+
+    def words_from_file(self, path):
+        with open(path) as f:
+            for line in f:
+                if not line.strip() or line.startswith('#'):
+                    continue
+                yield line.strip()
+
+    def retrieve_words(self, word, provider):
+        path = provider['path']
+        fmt = provider['format']
+
+        for i in range(999):
+            fpath = os.path.join(path, f'{word}_{i+1}.{fmt}')
+            if not os.path.exists(fpath):
+                break
+            yield fpath
+
+    def table(self):
+        good_words = self.words_from_file(self.good_word_path)
+        good_words = zip(good_words, cycle([True]))
+
+        bad_words = self.words_from_file(self.bad_word_path)
+        bad_words = zip(bad_words, cycle([False]))
+
+        for word, goodness in [*good_words, *bad_words]:
+            for provider in self.providers.values():
+                for file in self.retrieve_words(word, provider):
+                    yield goodness, word, file
+
+    def load(self):
+        table = list(self.table())
+
+        X = [n[1:] for n in table]
+        y = [n[0] for n in table]
+
+        return X, y

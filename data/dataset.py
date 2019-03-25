d92f2a0de78e6f9c691e23af87ad845a370633ab
Marian Tietz <tietz@informatik.uni-hamburg.de>
2019-03-25 22:41:00 +0100

    Commit dangling changes

--- data/dataset.py
+++ data/dataset.py
@@ -19,6 +19,9 @@ DEFAULT_PROVIDERS = {
 
 
 class SwearDataset:
+    """Low-level dataset. Provides path to sound files (X) and
+    corresponding label (y).
+    """
     def __init__(self, providers, good_word_path='data/good_words.txt',
             bad_word_path='data/bad_words.txt'):
         self.good_word_path = good_word_path
@@ -37,17 +40,18 @@ class SwearDataset:
         fmt = provider['format']
 
         for i in range(999):
-            fpath = os.path.join(path, f'{word}_{i+1}.{fmt}')
+            fpath = os.path.join(path, '{word}_{n}.{fmt}'.format(
+			word=word, n=i+1, fmt=fmt))
             if not os.path.exists(fpath):
                 break
             yield fpath
 
     def table(self):
         good_words = self.words_from_file(self.good_word_path)
-        good_words = zip(good_words, cycle([True]))
+        good_words = zip(good_words, cycle([False]))
 
         bad_words = self.words_from_file(self.bad_word_path)
-        bad_words = zip(bad_words, cycle([False]))
+        bad_words = zip(bad_words, cycle([True]))
 
         for word, goodness in [*good_words, *bad_words]:
             for provider in self.providers.values():
@@ -61,3 +65,26 @@ class SwearDataset:
         y = [n[0] for n in table]
 
         return X, y
+
+
+
+class SwearBinaryAudioDataset:
+    """Provides audio frames and a binary label (swear/noswear).
+    """
+    def __init__(self, X, y, parser):
+        self.X = X
+        self.y = y
+        assert len(X) == len(y)
+        self.parser = parser
+
+    def table(self):
+        for i, (word, fpath) in enumerate(self.X):
+            frames = self.parser.parse_audio(fpath)
+            yield frames, int(self.y[i])
+
+    def load(self):
+        Xy = list(self.table())
+        return (
+            [n[0] for n in Xy],
+            [n[1] for n in Xy],
+        )

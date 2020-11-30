9734006e05e1a3a30cd5b7744edb886a61c2f683
nemo <git@ningu.net>
2020-11-30 23:03:02 +0100

    Order. ORDER

--- noswear/model.py
+++ noswear/model.py
@@ -17,8 +17,9 @@ class NoSwearModel(torch.nn.Module):
         self,
         base_model,
         n_hidden=10,
-        n_layer=1,
-        p_dropout=0.2
+        n_layers=1,
+        p_dropout=0.2,
+        selector='last',
     ):
         super().__init__()
         self.base_model = base_model
@@ -27,68 +28,78 @@ class NoSwearModel(torch.nn.Module):
         self.base_model.fc = Identity()
         self.base_model.inference_softmax = Identity()
 
-        self.rnn = torch.nn.GRU(
-                672,
-                n_hidden,
-                num_layers=n_layer,
-                bias=False,
-                batch_first=True)
+        self.selector = selector
 
-        self.clf = torch.nn.Linear(n_hidden, 2, bias=False)
+        self.rnn = torch.nn.Sequential(
+            torch.nn.LSTM(672, n_hidden, num_layers=n_layers, bias=False, batch_first=True),
+            RNNValueExtractor(),
+        )
+
+        self.clf = torch.nn.Linear(n_hidden, 1, bias=False)
         self.dropout = torch.nn.Dropout(p=p_dropout)
 
-    def forward_step(self, X, lens, h=None):
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.rnn[0].reset_parameters()
+        self.clf.reset_parameters()
+
+    def forward(self, X, lens):
         # run base model, output is NxTxH with
         # T=Time, N=samples, H=hidden.
         y_pre = self.base_model(X)
         y_pre = self.dropout(y_pre)
 
         # run RNN over sequence and extract last item
-        y, h = self.rnn(y_pre, h)
+        y = self.rnn(y_pre)
 
-        # pick frame ith highest activation
-        i = y.mean(-1).argmax(1)
-        y = y[torch.arange(len(y)), i]
+        if self.selector == 'designated':
+            # instead of taking element -1 we just
+            # designate neuron 0 to be our confidence indicator;
+            # we take the element where [0] is greatest.
+            idcs_time = y[:, :, 0].argmax(axis=-1)
+            idcs_batch = list(range(X.shape[0]))
 
-        y = self.dropout(y)
+            # index using (batch, time) tuples
+            y = y[idcs_batch, idcs_time, :]
+        else:
+            y = y[:, -1]
 
-        # run classifier
         y = self.clf(y)
-        y = torch.softmax(y, dim=-1)
-        return y, h
-
-    def forward(self, X, lens):
-        y, _ = self.forward_step(X, lens)
         return y
 
 
 def get_net(base_model, device='cpu', **kwargs):
-    net = skorch.NeuralNetClassifier(
+    net = skorch.classifier.NeuralNetBinaryClassifier(
         partial(NoSwearModel, base_model),
 
         iterator_train=bucketing_dataloader,
         iterator_train__bucket_fn=bucket,
+        iterator_train__shuffle=True,
         iterator_valid=bucketing_dataloader,
         iterator_valid__bucket_fn=bucket,
 
-        batch_size=64,
-        max_epochs=40,
+        batch_size=4,
+        max_epochs=80,
         device=device,
 
-        module__p_dropout=0.2,
-        module__n_hidden=300,
+        train_split=predefined_split(Dataset(X_valid, y_valid)),
+
+        module__p_dropout=0.0,
+        module__n_hidden=32,
+        module__n_layers=1,
+        module__selector='designated',
 
         optimizer=torch.optim.Adam,
         optimizer__lr=0.0004,
 
         callbacks=[
             skorch.callbacks.Freezer('base_model.*'),
+            skorch.callbacks.Checkpoint(monitor='valid_acc_best'),
         ],
-
         **kwargs,
     )
     net.initialize()
-    return net
 
 
 def load_model(base_model, weights):
